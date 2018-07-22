c398b783639443a6a78deb59efc7a443140bad00
johngorithm <johngorithm@gmail.com>
2018-07-22 12:03:09 +0100

    On branch user-requests  Changes to be committed: 	modified:   css/register.css 	new file:   css/requests.css 	modified:   css/style.css 	modified:   home.html 	modified:   js/requests.js 	modified:   profile.html 	modified:   requests.html
    FEATURE <ui> : FETCH AND DISPLAY ALL REQUESTS MADE TO A USER'S RIDES
    
    This fetch and return all requests other users made  to the logged in
    user's ride offers
    
    FINISHED

--- js/requests.js
+++ js/requests.js
@@ -1,7 +1,5 @@
-(() => {
-   // LOG USER OUT OF APP
-  const logoutBtn = document.querySelector('nav .navbar ul.nav-right li a#logout');
-  logoutBtn.addEventListener('click', (event) => {
+const logout = (element) => {
+  element.addEventListener('click', (event) => {
     event.preventDefault();
 
     localStorage.removeItem('token');
@@ -9,4 +7,57 @@
 
     window.location.href = 'index.html';
   });
-})();
+}
+
+document.body.onload = () => {
+   // LOG USER OUT OF APP
+  const logoutBtn = document.querySelector('nav .navbar ul.nav-right li a#logout');
+  logout(logoutBtn)
+
+  // LOAD REQUESTS
+  const baseUrl = 'http://localhost:9000/api/v1';
+  const token = localStorage.getItem('token');
+  const requestsDomContainer = document.querySelector('main#request-loader');
+  if (token) {
+    fetch(`${baseUrl}/users/rides/requests`, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+        'x-access-token': token,
+      }
+    })
+    .then(response => {
+      return response.json();
+    }).then(data => {
+      let requestHtml = '';
+      if (data.requests) {
+        console.log(data.requests);
+        data.requests.forEach(request => {
+        requestHtml += `
+          <div id="${request.request_id}" class="row request">
+            
+            <div class="request-msg  co-xl-7 co-lg-7 co-md-12 co-sm-12">
+                <p class="small"> <strong>${request.sender}</strong> want to ride with you to <strong>${request.destination}</strong></p>
+            </div>
+            <div class="request-btns co-xl-5 co-lg-5 co-md-12 co-sm-12">
+                <button class="button button-white reject-btn">REJECT</button>
+                <button class="button button-blue accept-btn">ACCEPT</button>
+            </div>
+        </div>
+        `
+      });
+
+      requestsDomContainer.innerHTML = requestHtml;  
+      } else {
+        document.querySelector('main #loading').innerHTML = `${data.message}, Please login <p><a href="./login.html">LOGIN</a><p>`
+      }
+      
+
+    }).catch( error => {
+      document.querySelector('main #loading').innerHTML = `Error: ${error.message}`
+      throw new Error(error.message);
+    })
+  } else {
+    document.querySelector('main #loading').innerHTML = `You are not logged in, Please login <a href="./login.html">LOGIN</a>`    
+  }
+}
