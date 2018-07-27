229114c99650bf1ccdac8184661602dca09be416
johngorithm <johngorithm@gmail.com>
2018-07-27 07:21:02 +0100

    PREVENT USER FROM CREATING RIDE OFFERS WITH PAST DATE

--- js/general.js
+++ js/general.js
@@ -50,11 +50,65 @@ const logout = (element) => {
 
     const formFields = [destination, time, date, capacity, takeOffVenue];
     let isAllProvided = true;
+    const now = new Date();
+    console.log(now);
+
     formFields.forEach((field) => {
       if (!field.value) {
         inputError = '* required!';
         field.previousElementSibling.firstElementChild.textContent = inputError;
         isAllProvided = false;
+      } else if (field === date) {
+        const year = now.getFullYear();
+        const month = now.getMonth() + 1;
+        const day = now.getDate();
+        let currentDate = '';
+
+        if (month < 10 && day < 10) {
+          currentDate = `${year}-0${month}-0${day}`;
+        } else if (month < 10 && day > 9) {
+          currentDate = `${year}-0${month}-${day}`;
+        } else if (month > 9 && day < 10) {
+          currentDate = `${year}-${month}-0${day}`;
+        } else {
+          currentDate = `${year}-${month}-${day}`;
+        }
+        
+        if (currentDate > field.value ) {
+          inputError = 'date is past';
+          field.previousElementSibling.firstElementChild.textContent = inputError;
+          isAllProvided = false;
+          return true;
+        } else if (currentDate === field.value) {
+          field.previousElementSibling.firstElementChild.textContent = '';
+          if (time.value.length > 1) {
+            const currentHour = now.getHours();
+            const userInputedHour = parseInt(time.value);
+            if (currentHour > userInputedHour) {
+              time.previousElementSibling.firstElementChild.textContent = 'time is past';
+              isAllProvided = false;
+            } else if (currentHour === userInputedHour) {
+              const userInputedMinutes = Number(time.value.split(':')[1]);
+              const currentMinutes = (new Date()).getMinutes();
+              if (currentMinutes > userInputedMinutes) {
+                time.previousElementSibling.firstElementChild.textContent = 'time is past';
+                isAllProvided = false;
+              } else if (!(userInputedMinutes - currentMinutes > 20)) {
+                time.previousElementSibling.firstElementChild.textContent = 'take off time is less than 20 mins';
+                isAllProvided = false;
+              } else {
+                time.previousElementSibling.firstElementChild.textContent = '';
+              }
+            } else {
+              time.previousElementSibling.firstElementChild.textContent = '';
+            }
+          } else {
+            time.previousElementSibling.firstElementChild.textContent = '* required!';
+            isAllProvided = false;
+          }
+        } else {
+          field.previousElementSibling.firstElementChild.textContent = '';
+        }
       } else {
         field.previousElementSibling.firstElementChild.textContent = '';
       }
@@ -92,7 +146,7 @@ const logout = (element) => {
           messageOutput.style.color = 'rgb(10, 200, 32)';
           setTimeout(() => {
             window.location.href = 'home.html'
-          }, 2000)
+          }, 1000)
         } else if (data.message.includes('token')) {
           navRight.innerHTML = nav;
           messageOutput.innerHTML = '<p>Authentication Failed, Please <a style="text-decoration: none; color: dodgerblue;" href="./login.html">Login</a></p>';
